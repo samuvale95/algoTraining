ba2867c35ddd9e90c8c6a9598cb0ca262508a2a1
Samuele Valente <samuelevalente@gmail.com>
2021-10-03 15:58:44 +0200

    add two sum solution

---
+++ python/easy/two_sum/script.py
@@ -0,0 +1,18 @@
+def twoSum(nums, target):
+    hmap = {}
+
+    for i in range(0, len(nums)):
+        partial = target - nums[i]
+
+        if(partial in hmap.keys()):
+            return [nums[i], partial]
+
+        hmap[nums[i]] = i
+
+def main():
+    print(twoSum([2,7,11,15], 9))
+    print(twoSum([3,2,4], 6))
+    print(twoSum([3,3], 6))
+
+if __name__ == "__main__":
+    main()
